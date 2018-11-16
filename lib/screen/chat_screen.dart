8af80febb2050cb8f4521fc092e3cdaf713cdcde
unknown <giampieer24@gmail.com>
2018-11-16 16:08:51 -0500

    add textifield

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -9,11 +9,39 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  //sirve como certificador de como cambia de estado
+  final TextEditingController _textController = new TextEditingController();
+  Widget __buildTextComposer() {
+    return new Container(
+      child: new Row(
+        children: <Widget>[
+          new Flexible (
+            child:  new TextField(
+              controller: _textController,
+            ),
+
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //se recibe el contructor
-      child: new Text(widget.name),
+    return new Scaffold(
+        appBar:  new AppBar(
+          //se recibe el contructor
+          title: new Text(widget.name),
+        ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Container(
+              child: __buildTextComposer(),
+            )
+          ],
+        ),
+      )
     );
   }
 }
